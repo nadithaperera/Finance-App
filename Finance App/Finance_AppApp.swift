59b9fa739ceb8617749966e57f73a40f0e6784ac
Naditha <nadithag@gmail.com>
2023-09-16 09:50:53 +0530

    Initial Commit

---
+++ Finance App/Finance_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Finance_AppApp.swift
+//  Finance App
+//
+//  Created by Naditha on 2023-09-16.
+//
+
+import SwiftUI
+
+@main
+struct Finance_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
